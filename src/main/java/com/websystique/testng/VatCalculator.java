43fb0db87a5ca05007815aa20bf4985d67d3b944
Meirzhan_Rymbayev <meirzhan_rumbaev@mail.ru>
2016-03-31 15:09:20 +0600

    Initial commit. TestNG HelloWorld example

---
+++ src/main/java/com/websystique/testng/VatCalculator.java
@@ -0,0 +1,13 @@
+package com.websystique.testng;
+
+/**
+ * Created by Meirzhan_Rymbayev on 3/31/2016.
+ */
+public class VatCalculator {
+    /*
+    * Returns 21% VAT on given amount
+    */
+    public double getVatOnAmount(double amount) {
+        return amount * 0.21;
+    }
+}
